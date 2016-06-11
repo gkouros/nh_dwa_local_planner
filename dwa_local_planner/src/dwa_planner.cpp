8bfcdcb898cc5fcc90bab54ee8e662b47769cd98
George Kouros <gkourosg@yahoo.gr>
2016-06-11 19:18:21 +0300

    Remove redundant loop for vector copying

--- dwa_local_planner/src/dwa_planner.cpp
+++ dwa_local_planner/src/dwa_planner.cpp
@@ -247,11 +247,9 @@ namespace dwa_local_planner {
 
   void DWAPlanner::updatePlanAndLocalCosts(
       tf::Stamped<tf::Pose> global_pose,
-      const std::vector<geometry_msgs::PoseStamped>& new_plan) {
-    global_plan_.resize(new_plan.size());
-    for (unsigned int i = 0; i < new_plan.size(); ++i) {
-      global_plan_[i] = new_plan[i];
-    }
+      const std::vector<geometry_msgs::PoseStamped>& new_plan)
+  {
+    global_plan_ = new_plan;
 
     // costs for going away from path
     path_costs_.setTargetPoses(global_plan_);
